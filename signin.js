cf67082437dafb368b4a3b54afaec3d619fbb0de
veronica <veronicaddsy@gmail.com>
2024-05-01 01:31:11 +0800

    changed the input box and added new css style

--- signin.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-    var email = document.getElementById('user_email').value;
-    var password = document.getElementById('user_pw').value;
-    if (email == "" || password == "") {
-        alert('Both email and password are required to log in.');
-        return false;
-    }
-    return true;
-}
