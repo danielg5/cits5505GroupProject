2104c45650a85e3b007cb37642d2b9b2b2c17ec6
Asad <asadmaza@hotmail.com>
2024-05-05 20:39:29 +0800

    Updated information button popout box using modal with CSS keyframes animation

--- menu.js
+++ menu.js
@@ -6,10 +6,6 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
 });
 
-function showInfoModal() {
-    alert("Menu options include: Username, Play a Random Game, High Scores, Search for a Theme, Create a Theme.");
-}
-
 function logout() {
     // TODO - Add necessary cleanup operations before logout
     // TODO - Redirect to index.html page after logout
