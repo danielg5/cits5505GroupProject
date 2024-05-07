d50bc94448eab29d5d334a2856934e23a7460e48
Daniel Gal <danielgal8@hotmail.com>
2024-05-07 19:07:56 +0800

    additional entries to fix CORS-related error in browser

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 flaskApp = Flask(__name__)
+CORS(flaskApp)  # enable CORS on all routes
 flaskApp.config.from_object(Config)
 db = SQLAlchemy(flaskApp)
 migrate = Migrate(flaskApp, db)
 
-from app import routes, model
+from app import routes, model, gameplay
