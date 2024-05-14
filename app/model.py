a3188b9735ca1109e7f867f7aa2511fe58cbbab9
Daniel Gal <danielgal8@hotmail.com>
2024-05-14 19:25:30 +0800

    fixed circular error with __init__.py and model.py on flask run

--- app/model.py
+++ app/model.py
@@ -1,6 +1,6 @@
-from app import db, login
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
 import random, string
 
 class Person(UserMixin, db.Model):
