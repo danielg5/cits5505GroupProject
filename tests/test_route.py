1b55743d7bf6425ad2cc86783a452ab436034c07
maxchtse <tsechunghei97@outlook.com>
2024-05-19 16:41:06 +0800

    Update test cases

--- tests/test_route.py
+++ tests/test_route.py
@@ -1,5 +1,7 @@
+# test_routes.py
+
 import unittest
-from flask import Flask, session
+from flask import Flask
 from app.routes import flaskApp
 from app import db
 
@@ -9,32 +11,38 @@ class TestIndexRoute(unittest.TestCase):
         self.app = flaskApp.test_client()
         self.app.testing = True
         flaskApp.config['WTF_CSRF_ENABLED'] = False
-        flaskApp.config['SECRET_KEY'] = 'mysecret'
 
+    # Test Case 1: Test a user successfully login the account
     def test_index_route_login(self):
         # Simulate a POST request to test login
         response = self.app.post('/', data={'email': 'test@uwa.com', 'password': 'testpass'}, follow_redirects=True)
         # Check for redirection status code
         self.assertEqual(response.status_code, 200)
-        # Check for expected content indicating redirection to the game page
-        self.assertIn('Game Page', response.data.decode())
+        # Check for redirection to the gaming page
+        self.assertTrue('/menu' in response.request.path)
 
+    # Test Case 2: Test a user failed to login the account
     def test_index_route_login_fail(self):
         # Simulate a POST request to test login failure with incorrect credentials
         response = self.app.post('/', data={'email': 'wrong@example.com', 'password': 'wrong'}, follow_redirects=True)
         # Check that the response does not redirect to the game page
-        self.assertNotIn('Game Page', response.data.decode())
+        self.assertFalse('/game' in response.request.path)
         # Ensure the status code is 200, implying that the user stays on the index page
         self.assertEqual(response.status_code, 200)
 
+    # Test Case 3: Test a user successfully logout from the account
     def test_logout_route(self):
         # First log in to create a session
-        self.app.post('/', data={'email': 'test@uwa.com', 'password': 'testpass'}, follow_redirects=True)
+        login_response = self.app.post('/', data={'email': 'test@uwa.com', 'password': 'testpass'}, follow_redirects=True)
+        # Verify login was successful before testing logout
+        self.assertIn('/menu', login_response.request.path) 
         # Now attempt to log out
         response = self.app.get('/logout', follow_redirects=True)
         # Check that the user is redirected to the index page
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Login Page', response.data.decode())  # Assuming the index page has "Login Page" text
+        self.assertTrue('/' in response.request.path)
+
+
 
 if __name__ == '__main__':
     unittest.main()
